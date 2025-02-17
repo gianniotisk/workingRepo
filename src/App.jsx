c5d518544442867a129be2539e6f72adff479933
gianniotisk <ukostasu@gmail.com>
2025-02-17 19:12:52 +0200

    Add login function

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Website-Pages/Page_Home"; // Home Page Component
 import About from "./Website-Pages/About"; // About Page Component
 import Bookmarks from "./Website-Pages/Page_Bookmark"; //Bookmarks Page Component
+import Login from "./Login/Login"; //Login Page Component
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/bookmarks" element={<Bookmarks />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/Website-Pages/Page_Login.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./Page_Login.css";
+
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+
+export default function App() {
+  return (
+    <div className="">
+      <Header />
+      <Login />
+      <Footer />
+    </div>
+  );
+}
